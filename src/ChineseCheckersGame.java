b587c2027965fc968e81a313008e1c82a6fec0c8
soubeyju <soubeyju@PC-B123-20.iut-valence.fr>
2016-02-18 12:01:39 +0100

    Soubeyrand Jules Prevost Nicolas
    we created : 
     - the application Main
     - the first class ChineseCheckersGame

---
+++ src/ChineseCheckersGame.java
@@ -0,0 +1,16 @@
+/**
+ * it represent a game of chinese checkers.
+ * @author soubeyju
+ */
+public class ChineseCheckersGame
+{
+
+	/**
+	 * it represent the game's initialization.
+	 */
+	public void startGame()
+	{
+		
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ChineseCheckersGame chineseCheckers = new ChineseCheckersGame();
+		chineseCheckers.startGame();
+		
+	}
+}
