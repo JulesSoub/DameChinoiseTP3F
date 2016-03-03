763e299383c2001ea4ecb73276b15c3129a303eb
soubeyju <soubeyju@PC-A213-07.iut-valence.fr>
2016-03-03 12:00:50 +0100

    We create a class Board, We made the constructor's and operation's commit !

---
+++ src/Board.java
@@ -0,0 +1,16 @@
+
+/**
+ * this class represents a chinese checkers game board.
+ * @author soubeyju
+ *
+ */
+public class Board
+{
+	/**
+	 * Creates a new game board, in its starting configuration.
+	 */
+	public Board()
+	{
+		
+	}
+}
--- src/ChineseCheckersGame.java
+++ src/ChineseCheckersGame.java
@@ -1,16 +1,27 @@
 /**
- * it represent a game of chinese checkers.
+ * it represents a game of chinese checkers.
  * @author soubeyju
  */
 public class ChineseCheckersGame
 {
+	private Board board;
 
 	/**
-	 * it represent the game's initialization.
+	 * Creates a new Chinese checkers game, ready to be played (game board is in its 
+	 * starting configuration).
 	 */
-	public void startGame()
+	public ChineseCheckersGame()
 	{
-		
+		this.board = new Board();
 	}
-
-}
+	
+	/**
+	 * Plays the game (It's a )
+	 */
+	public void play()
+	{
+		System.out.println("Game started");
+		System.out.println("Game finished");
+	}
+	
+}     
