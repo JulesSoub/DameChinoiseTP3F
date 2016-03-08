6ab7d9e184fc8df307fab0b6ec95439ea719bb49
soubeyju <soubeyju@PC-A213-07.iut-valence.fr>
2016-03-08 12:31:14 +0100

    We Modified, added and completed some comit. We simplified the application. We answered at the "TODO" that the teacher wrote.

--- src/ChineseCheckersGame.java
+++ src/ChineseCheckersGame.java
@@ -1,16 +1,15 @@
-// TODO update comment (find a link to a description of the game in english)
+// TODO (DONE) update comment (find a link to a description of the game in english)
 /**
- * it represents a game of chinese checkers.
+ * it represents a game of chinese checkers. It's a One-on-one Game between 2 (or 
+ * more, until 8 persons) persons. The object of the game is to put your pawns in the 
+ * opposite camp in front of yours.
  * @author soubeyju
  */
 public class ChineseCheckersGame
 {
 	private Board board;
 
-	// TODO write ChineseCheckersGame()
-	
-	// TODO rename this method (ask for advice)
-	// TODO update comment (what is the main algorithm?)
+	// TODO (DONE) write ChineseCheckersGame()
 	/**
 	 * Creates a new Chinese checkers game, ready to be played (game board is in its 
 	 * starting configuration).
@@ -20,8 +19,20 @@ public class ChineseCheckersGame
 		this.board = new Board();
 	}
 	
+	// TODO (DONE) rename this method (ask for advice)
+	// TODO (DONE) update comment (what is the main algorithm?)
 	/**
-	 * Plays the game (It's a )
+	 * Plays the game (Repetitive structure as long as ALL your pawns aren't in the opposite 
+	 * camp :
+	 * 		- Movements (6 possibilities, with the possibility to jump over a pawn))
+	 * 
+	 * While(pawn aren't in the opposite camp)
+	 * 	{
+	 * 		If (it's your turn)
+	 * 			moveYourPawn();
+	 * 		Else 
+	 * 		 	moveOpponentPawn();
+	 *  }
 	 */
 	public void play()
 	{
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 // TODO update README.md (what/who)
 
 
-// TODO write comment
+// TODO (DONE) write comment
+
+/**
+ * It's an application which permits to play a chinese chekers game. 
+ * @author soubeyju
+ *
+ */
 public class Main
 {
 	// TODO write comment
+	
+	/**
+	 *  
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-		// TODO simplify by avoiding to use a local variable
-		ChineseCheckersGame chineseCheckers = new ChineseCheckersGame();
-		chineseCheckers.play();
+		// TODO (DONE) simplify by avoiding to use a local variable
+		new ChineseCheckersGame().play();
 		
 	}
 }
